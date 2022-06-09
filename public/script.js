bcd478c8b176e6726a7ed317ae314d995ad1908f
chepark <chaeahpark.kr@gmail.com>
2022-06-09 13:52:07 +0300

    Adding the script.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,50 @@
 "use strict";
+const calcContainer = document.querySelector(".calc-container");
+const logo = document.querySelector(".logo");
+const calcAnswerBox = (document.querySelector(".calc-answer-box"));
+const calcAnswer = document.querySelector(".calc-answer");
+const themeOptions = document.querySelector(".theme-options");
+const themeButtons = Array.from(document.getElementsByClassName("theme-option"));
+const keyboardBox = document.querySelector(".calc-keyboard");
+const numButtons = Array.from(document.getElementsByClassName("button-norm"));
+// Theme controller
+themeButtons.forEach((themeBtn) => {
+    themeBtn.addEventListener("click", () => { });
+});
+let prevNum;
+let currNum;
+let operator;
+let isOperatorOn = false;
+let answer = 0;
+numButtons.forEach((numBtn) => {
+    numBtn.addEventListener("click", () => {
+        if (isOperatorOn == false) {
+            calcAnswer.innerText == "0"
+                ? (calcAnswer.innerText = numBtn.value)
+                : (calcAnswer.innerText += numBtn.value);
+            prevNum = parseFloat(numBtn.value);
+        }
+    });
+});
+const displayCalculation = () => { };
+const deleteLastInput = () => { };
+const resetCalc = () => {
+    prevNum = 0;
+    currNum = null;
+    isOperatorOn = false;
+};
+const calculate = (operator) => {
+    if (prevNum && currNum)
+        switch (operator) {
+            case "+":
+                return (answer = prevNum + currNum);
+            case "-":
+                return (answer = prevNum - currNum);
+            case "/":
+                return (answer = prevNum / currNum);
+            case "*":
+                return (answer = prevNum * currNum);
+            default:
+                return 0;
+        }
+};
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,62 @@
+const calcContainer = <HTMLDivElement>document.querySelector(".calc-container");
+const logo = <HTMLHeadingElement>document.querySelector(".logo");
+const calcAnswerBox = <HTMLDivElement>(
+  document.querySelector(".calc-answer-box")
+);
+const calcAnswer = <HTMLDivElement>document.querySelector(".calc-answer");
+const themeOptions = document.querySelector(".theme-options");
+const themeButtons: Element[] = Array.from(
+  document.getElementsByClassName("theme-option")
+);
+const keyboardBox = <HTMLDivElement>document.querySelector(".calc-keyboard");
+const numButtons = Array.from(
+  document.getElementsByClassName("button-norm")
+) as HTMLInputElement[];
 
+// Theme controller
+themeButtons.forEach((themeBtn) => {
+  themeBtn.addEventListener("click", () => {});
+});
+
+let prevNum: number | null;
+let currNum: number | null;
+let operator: string;
+let isOperatorOn: boolean = false;
+let answer: number = 0;
+
+numButtons.forEach((numBtn) => {
+  numBtn.addEventListener("click", () => {
+    if (isOperatorOn == false) {
+      calcAnswer.innerText == "0"
+        ? (calcAnswer.innerText = numBtn.value)
+        : (calcAnswer.innerText += numBtn.value);
+      prevNum = parseFloat(numBtn.value);
+    }
+  });
+});
+
+const displayCalculation = () => {};
+
+const deleteLastInput = (): void => {};
+
+const resetCalc = (): void => {
+  prevNum = 0;
+  currNum = null;
+  isOperatorOn = false;
+};
+
+const calculate = (operator: string): number | undefined => {
+  if (prevNum && currNum)
+    switch (operator) {
+      case "+":
+        return (answer = prevNum + currNum);
+      case "-":
+        return (answer = prevNum - currNum);
+      case "/":
+        return (answer = prevNum / currNum);
+      case "*":
+        return (answer = prevNum * currNum);
+      default:
+        return 0;
+    }
+};
